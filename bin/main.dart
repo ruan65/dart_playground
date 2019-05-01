6a2086f34eab2dabe157c89ddae6de6efdd867bf
a <ruan65@gmail.com>
2019-05-01 21:56:26 +0300

    bicycle

--- bin/main.dart
+++ bin/main.dart
@@ -1,28 +1,54 @@
 import 'dart:async';
 
 main(List<String> arguments) async {
+//
+//  loadString_('privet').then((s) => print(s));
+//
+//
+//  loadString("hello").then((s) =>  print(s));
+//
+//
+//  addAsync(30)
+//      .then(
+//        (x) => x(50),
+//      )
+//      .then(
+//        (y) => print(y),
+//      );
+//
+//  Function x = await addAsync(77);
+//  int z = await x(23);
+//  print(z);
 
-  var f_loadedString = loadString_('privet');
+  exampleStream(20).listen((i) => print('heard $i'));
 
-  String str = await f_loadedString;
-
-  print(str);
-
-  String str2 = await loadString("hello");
-  print(str2);
-
-
-
+//  await for(var s in stream) {
+//    print('Number $s');
+//  }
 
 }
 
+Stream<int> exampleStream(int x) async* {
+
+  for(int i = 0; i <= x; i++) {
+
+    await Future.delayed(Duration(milliseconds: 300)).then((_) => print('after await $i'));
+
+    yield i;
+  }
+}
+
 Future<String> loadString(String str) async {
-  await Future.delayed(Duration(seconds: 2));
+  await Future.delayed(Duration(seconds: 1));
   return "String: $str";
 }
 
 Future<String> loadString_(String str) {
-  return Future.delayed(Duration(seconds: 3)).then((_) {
+  return Future.delayed(Duration(seconds: 2)).then((_) {
     return 'String: $str';
   });
+}
+
+Future<Function> addAsync(int x) async {
+  return (int y) async => x + y;
 }
--- lib/lang_tutorial.dart
+++ lib/lang_tutorial.dart
@@ -1,7 +1,7 @@
 
-//import 'dart:async';
-//import 'package:lang_tutorial/lang_tutorial.dart';
-//
-//void main(List<String> args) {
-//  print("hello there.....");
-//}
+import 'dart:async';
+import 'package:lang_tutorial/lang_tutorial.dart';
+
+void main(List<String> args) {
+ print("hello there.....");
+}
