68b4d5a2c833801c0d20458035d73fdbf47ef81f
a <ruan65@gmail.com>
2019-05-02 15:14:19 +0300

    rectangle

---
+++ bin/rectangle/rectangle.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+class Rectangle {
+  int width;
+  int height;
+  Point origin;
+
+  Rectangle({
+    this.origin = const Point(0, 0),
+    this.width = 0,
+    this.height = 0,
+  });
+
+  @override
+  String toString() =>
+      "Origin: (${origin.x}, ${origin.y}, width: $width, height: $height)";
+}
+
+main(List<String> args) {
+  var rect = Rectangle(
+    origin: Point(10, 10),
+    width: 50,
+    height: 25,
+  );
+  print(rect);
+}
